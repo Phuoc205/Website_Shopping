b5fd517aea42270f66161eecc3837e6fe0c98a86
Phu Ha <minhphu2020py@gmail.com>
2025-04-19 18:21:07 +0700

    main

---
+++ src/database/insert_products/insert_keyboard.sql
@@ -0,0 +1,29 @@
+-- ************************************************************** --
+-- **************************INSERT KEYBOARD********************* --
+-- ************************************************************** --
+
+INSERT INTO CATEGORY_KEYBOARD 
+(
+  PRODUCT_ID, SWITCH, MODEL, KEYBOARD_SIZE, CONNECTION_RANGE,
+  KEYCAP_MATERIAL, KEYBOARD_WEIGHT, RGB, NUM_KEYS
+)
+VALUES 
+('KEYB001', 'Cherry MX Red', 'Ducky One 2 TKL', 'Tenkeyless (TKL)', 10, 'PBT', 900, 'RGB', 87),
+
+('KEYB002', 'Razer Green', 'Razer BlackWidow V3', 'Fullsize', 0, 'ABS', 1100, 'RGB Chroma', 104),
+
+('KEYB003', 'Gateron Brown', 'Keychron K6', '65%', 15, 'PBT', 650, 'RGB', 68),
+
+('KEYB004', 'Kailh Choc', 'Logitech MX Mechanical', 'Low Profile', 10, 'ABS', 750, 'White Backlight', 84),
+
+('KEYB005', 'Outemu Blue', 'Royal Kludge RK61', '60%', 10, 'ABS', 600, 'RGB', 61),
+
+('KEYB006', 'Membrane', 'Logitech K270', 'Fullsize', 10, 'ABS', 500, 'None', 104),
+
+('KEYB007', 'Gateron Yellow', 'Akko 3068B', '65%', 15, 'PBT', 800, 'RGB', 68),
+
+('KEYB008', 'Cherry MX Brown', 'Kinesis Advantage2', 'Split', 0, 'PBT', 1200, 'None', 95),
+
+('KEYB009', 'Kailh Box White', 'Keychron Q1', '75%', 0, 'PBT', 1000, 'RGB South-facing', 81),
+
+('KEYB010', 'Cherry MX Silent Red', 'Leopold FC660C', '65%', 0, 'PBT', 900, 'White LED', 66);
