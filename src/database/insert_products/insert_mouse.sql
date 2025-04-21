ce44674ae09d4ac0698732fb3c1ea698d51fee01
Phu Ha <minhphu2020py@gmail.com>
2025-04-21 20:31:46 +0700

    main

---
+++ src/database/insert_products/insert_mouse.sql
@@ -0,0 +1,31 @@
+-- ************************************************************** --
+-- **************************INSERT MOUSE************************ --
+-- ************************************************************** --
+
+INSERT INTO CATEGORY_MOUSE 
+(
+    PRODUCT_ID, SWITCH, IPS, LED, LIFESPAN, RESPOND_RATE,
+    DESIGN, BATTERY_LIFE, DIMENSION, MOUSE_WEIGHT, COLOR,
+    TYPE_CONNECT, CONNECTION_RANGE, SENSOR, BUTTONS
+)
+
+VALUES 
+('MOUSE001', 'Omron', 400, 'RGB', 50000000, 1, 'Ergonomic', 70, '125 x 65 x 40 mm', 85, 'Black', 'Wireless (USB Receiver)', 10, 'Optical', 6),
+
+('MOUSE002', 'Huano', 300, 'Red', 20000000, 2, 'Ambidextrous', 60, '120 x 63 x 38 mm', 78, 'White', 'Wired (USB)', 0, 'Laser', 5),
+
+('MOUSE003', 'Kailh', 650, 'RGB', 80000000, 1, 'Claw Grip', 90, '130 x 70 x 42 mm', 95, 'Gray', 'Wireless + Wired', 15, 'Optical', 8),
+
+('MOUSE004', 'Omron', 500, 'Blue', 50000000, 1, 'Vertical', 80, '115 x 60 x 45 mm', 100, 'Black', 'Bluetooth', 10, 'Optical', 7),
+
+('MOUSE005', 'TTC', 400, 'RGB', 60000000, 1, 'Gaming', 50, '118 x 64 x 39 mm', 88, 'Red', 'Wired (USB)', 0, 'Laser', 9),
+
+('MOUSE006', 'Razer Green', 650, 'RGB Chroma', 70000000, 1, 'Ergonomic', 90, '126 x 66 x 38 mm', 82, 'Green', 'Wireless + Wired', 12, 'Optical', 7),
+
+('MOUSE007', 'Logitech D2FC', 300, 'White', 50000000, 2, 'Symmetrical', 60, '121 x 62 x 40 mm', 75, 'Blue', 'Wired (USB)', 0, 'Laser', 6),
+
+('MOUSE008', 'Omron', 450, 'RGB', 50000000, 1, 'Palm Grip', 85, '129 x 68 x 41 mm', 89, 'Black-Red', 'Wireless (USB Receiver)', 10, 'Optical', 8),
+
+('MOUSE009', 'Kailh GM8.0', 600, 'RGB', 80000000, 1, 'Ergonomic', 100, '135 x 70 x 43 mm', 92, 'Silver', 'Bluetooth', 15, 'Optical', 9),
+
+('MOUSE010', 'Huano Blue', 350, 'Green', 30000000, 1, 'Fingertip Grip', 50, '117 x 60 x 38 mm', 76, 'White-Gray', 'Wired (USB)', 0, 'Laser', 5);
