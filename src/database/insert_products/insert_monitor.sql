4f8cb6de78b09767cd06334b713aa22d3f3988ff
Phu Ha <minhphu2020py@gmail.com>
2025-04-23 19:31:01 +0700

    main

---
+++ src/database/insert_products/insert_monitor.sql
@@ -0,0 +1,26 @@
+INSERT INTO CATEGORY_MONITOR 
+(
+    PRODUCT_ID, DIMENSION, RESOLUTION, REFRESH_RATE, RESPOND_TIME,
+    SCREEN_RATIO, STATIC_CONTRAST, BRIGHTNESS, COLOR_SPACE,
+    PANEL, TYPE_SCREEN, MONTITOR_WEIGHT
+)
+VALUES
+('MON001', '614 x 364 x 51 mm', '3840 x 2160', 144, 1, '16:9', '1000:1', 400, 'DCI-P3 98%', 'IPS', 'Flat', 7500),
+
+('MON002', '563 x 329 x 51 mm', '1920 x 1080', 280, 1, '16:9', '1000:1', 400, 'sRGB 99%', 'IPS', 'Flat', 5400),
+
+('MON003', '709 x 512 x 248 mm', '1920 x 1080', 165, 1, '16:9', '3000:1', 250, 'NTSC 72%', 'VA', 'Curved', 7200),
+
+('MON004', '540 x 413 x 200 mm', '1920 x 1080', 75, 4, '16:9', '3000:1', 250, 'NTSC 72%', 'VA', 'Flat', 3400),
+
+('MON005', '539 x 322 x 42 mm', '1920 x 1080', 75, 5, '16:9', '1000:1', 250, 'sRGB 99%', 'IPS', 'Flat', 3700),
+
+('MON006', '614 x 367 x 65 mm', '2560 x 1440', 180, 0.5, '16:9', '1000:1', 350, 'DCI-P3 95%', 'IPS', 'Flat', 5300),
+
+('MON007', '714 x 427 x 55 mm', '3840 x 2160', 144, 1, '16:9', '1000:1', 400, 'DCI-P3 95%', 'IPS', 'Flat', 7400),
+
+('MON008', '715 x 415 x 63 mm', '3840 x 2160', 144, 1, '16:9', '1000:1', 400, 'DCI-P3 98%', 'IPS', 'Flat', 8500),
+
+('MON009', '714 x 432 x 99 mm', '2560 x 1440', 270, 0.5, '16:9', '1000:1', 400, 'DCI-P3 90%', 'IPS', 'Flat', 7900),
+
+('MON010', '723 x 434 x 94 mm', '2560 x 1440', 170, 1, '16:9', '3000:1', 250, 'sRGB 120%', 'VA', 'Curved', 7600);
